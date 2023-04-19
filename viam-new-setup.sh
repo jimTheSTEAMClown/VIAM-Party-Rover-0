8baa28ddb70a51cf7ee51f75506315ee6f24101d
Jim Burnham <jim.the.steam.clown@gmail.com>
2023-04-18 20:25:25 -0700

    Update viam-new-setup.sh

--- viam-new-setup.sh
+++ viam-new-setup.sh
@@ -2,13 +2,23 @@
 # https://docs.viam.com/installation/#detailed-installation-instructions
 cd 
 pwd
+echo "-------------------------------------------------------"
+echo " update & upgrade "
+echo "-------------------------------------------------------"
 sudo apt update
 sudo apt upgrade -y
+echo "-------------------------------------------------------"
+echo " Installing curl"
+echo "-------------------------------------------------------"
 sudo apt install curl -y
+uname -m
+echo "-------------------------------------------------------"
+echo " Setting up the vial-server - this service will run on bootup."
+echo "-------------------------------------------------------"
 curl http://packages.viam.com/apps/viam-server/viam-server-stable-aarch64.AppImage -o viam-server &&
   chmod 755 viam-server && sudo ./viam-server --aix-install
- curl http://packages.viam.com/apps/viam-server/viam-server-latest-aarch64.AppImage -o viam-server &&
-  chmod 755 viam-server && sudo ./viam-server --aix-install
-  echo "-------------------------------------------------------"
-  echo " put the start viam-server command here"
-  echo "-------------------------------------------------------"
+echo "-------------------------------------------------------"
+echo " Starting the viam-server"
+echo "-------------------------------------------------------"
+sudo systemctl start viam-server
+echo "Done"
